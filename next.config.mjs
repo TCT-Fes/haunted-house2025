5890bbe9ccc09bc7219631beb2d34c9c08775e44
physics11688 <t.yamamoto116@gmail.com>
2025-10-22 07:34:59 +0900

    ビルドテスト

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,12 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // 静的HTMLを出力
-  images: { unoptimized: true }, // 画像最適化を無効（GitHub Pages向け）
-  basePath: '/haunted-house2025', // ← リポジトリ名を指定（必須）
-  assetPrefix: '/haunted-house2025/', // ← 同じくリポジトリ名を指定（必須）
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
+  output: 'export', // ← これが重要！
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/haunted-house2025', // ← GitHub Pages用
+  assetPrefix: '/haunted-house2025/',
 };
 
 export default nextConfig;
